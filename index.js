dc1c86264d9a3cfeeca72fcab0e7a4f9ceef8b0d
sheldoncoates <sheldon.coates@rci.rogers.ca>
2019-09-16 12:06:35 -0400

    snek is stupid

--- index.js
+++ index.js
@@ -53,21 +53,16 @@ app.post('/move', (request, response) => {
   var dir = '';
 
   // move vertically
-  // if (foodY > snekHeadY) {
-  //   dir = 'up';
-  // } else if (foodY < snekHeadY) {
-  //   dir = 'down';
-  // } else { // same line as food, move left or right
-  //   if (foodX > snekHeadX) {
-  //     dir = 'right';
-  //   } else if (foodX < snekHeadX) {
-  //     dir = 'left';
-  //   }
-  // }
-
-  dir = 'up';
-  if(foodY > snekHeadY){
+  if (foodY > snekHeadY) {
+    dir = 'up';
+  } else if (foodY < snekHeadY) {
     dir = 'down';
+  } else { // same line as food, move left or right
+    if (foodX > snekHeadX) {
+      dir = 'right';
+    } else if (foodX < snekHeadX) {
+      dir = 'left';
+    }
   }
 
   // Response data for movin'
